41e2d4e74c9c479457591bba5eb07cbe439080e4
sinewyk <sinewyk@gmail.com>
2022-01-15 00:11:28 +0100

    emanations

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let registered = [];
+	let emanations = [];
+
+	const key_down_handler = (e: KeyboardEvent) => {
+		switch (e.key) {
+			case 'ArrowUp':
+				emanations.push('⬆');
+				break;
+			case 'ArrowDown':
+				emanations.push('⬇');
+				break;
+			case 'ArrowLeft':
+				emanations.push('⬅');
+				break;
+			case 'ArrowRight':
+				emanations.push('➡');
+				break;
+			case 'Enter':
+				registered.push(emanations.join(''));
+				emanations = [];
+				registered = registered;
+				break;
+			default:
+				return;
+		}
+		emanations = emanations;
+	};
+</script>
+
+<h1 class="mx-auto text-center font-bold mb-6">Emanations</h1>
+
+<div class="max-w-xl mx-auto">
+	<label for="emanations">Emanations (push arrows and press enter)</label>
+	<input class="block mb-3" name="emanations" type="text" on:keydown={key_down_handler} />
+
+	<ol>
+		{#each registered as emanation}
+			<li>{emanation}</li>
+		{/each}
+	</ol>
+</div>
