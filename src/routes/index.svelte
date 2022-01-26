67634187a33c109a78c37f209ab042c529523752
Serge Havas <sinewyk@gmail.com>
2022-01-26 20:21:59 +0100

    BIGGER

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,7 @@
 	let registered = [];
 	let emanations = [];
 
-	const key_down_handler = (e: KeyboardEvent) => {
+	function key_down_handler(e: KeyboardEvent) {
 		switch (e.key) {
 			case 'ArrowUp':
 				emanations.push('⬆');
@@ -17,26 +17,39 @@
 				emanations.push('➡');
 				break;
 			case 'Enter':
-				registered.push(emanations.join(''));
-				emanations = [];
-				registered = registered;
+				if (emanations.length !== 0) {
+					registered.push(emanations.join(''));
+					emanations = [];
+					registered = registered;
+				}
 				break;
 			default:
+				e.preventDefault();
 				return;
 		}
 		emanations = emanations;
-	};
+	}
+
+	function clear() {
+		registered = [];
+		emanations = [];
+	}
 </script>
 
-<h1 class="mx-auto text-center font-bold mb-6">Emanations</h1>
+<h1 class="mx-auto text-center font-bold mb-6 text-4xl">Emanations</h1>
 
-<div class="max-w-xl mx-auto">
-	<label for="emanations">Emanations (push arrows and press enter)</label>
-	<input class="block mb-3" name="emanations" type="text" on:keydown={key_down_handler} />
+<div class="max-w-xl mx-auto flex flex-col items-center">
+	<label class="text-lg mb-3 text-center" for="emanations"
+		>Emanations (push arrows and press enter)</label
+	>
+	<!-- svelte-ignore a11y-autofocus -->
+	<input autofocus class="block mb-3" name="emanations" type="text" on:keydown={key_down_handler} />
 
-	<ol>
+	<ol class="text-4xl text">
 		{#each registered as emanation}
-			<li>{emanation}</li>
+			<li class="mb-2">{emanation}</li>
 		{/each}
 	</ol>
+
+	<button on:click={clear}>Clear</button>
 </div>
